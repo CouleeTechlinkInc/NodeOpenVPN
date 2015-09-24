2e9c90fa8744bdb2bff541576ecd11b0987bc3c0
tholum <tim@couleetechlink.com>
2015-09-24 17:20:13 -0500

    Less and Stuff

--- www/js/openvpnmanager.js
+++ www/js/openvpnmanager.js
@@ -2,7 +2,9 @@ var OpenVPNManager = function(){
   var self = this;
   self.auth = new Auth();
   self.pages = ko.observableArray(
-    { "id" : "clients" , "displayName" : "Clients"}
+    [
+      { "id" : "clients" , "displayName" : "Clients"}
+    ]
   )
   self.showWizard = ko.observable(false);
   self.createClientConfig = function(params){

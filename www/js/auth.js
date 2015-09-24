f8bca87f528f493e83edea6ebbc0289ffe140783
tholum <tim@couleetechlink.com>
2015-09-23 20:23:26 -0500

    Install File

--- www/js/auth.js
+++ www/js/auth.js
@@ -9,6 +9,7 @@ var Auth = function(){
       self.baseHash( CryptoJS.SHA256(  self.username() + '' +  self.password()  ).toString() );
       self.password(''); // Clear the password it is no loner needed
       socket.emit('checkAuth' , self.username() );
+      
   }
   self.sendLogin = function( challange ){
     //Hash this with the challenge to create a token that is unique to prevent playback attacks

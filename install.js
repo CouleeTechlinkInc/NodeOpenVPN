b2e4d1b348ad49e2d7b753daf06533612d6947de
tholum <tim@couleetechlink.com>
2015-09-23 20:14:26 -0500

    Install File

--- install.js
+++ install.js
@@ -9,15 +9,17 @@
 var prompt = require('prompt');
 var schema = {
     properties: {
+      displayName : {
+        pattern: /^[a-zA-Z1-9 ]+$/,
+        message: 'your Name',
+        required: true
+      },
       username: {
         pattern: /^[a-zA-Z1-9]+$/,
-        message: 'Name must be only letters, spaces, or dashes',
+        message: 'Name must be only letters or numbers',
         required: true
       },
       password: {
-        hidden: true
-      },
-      rePassword: {
         hidden: true
       }
     }
@@ -28,7 +30,16 @@ var schema = {
   // Get two properties from the user: username and email
   //
   prompt.get(schema, function (err, result) {
+    var sha256 = function( string ){
+      return crypto.createHash('sha256').update( string ).digest('hex') ;
+    }
+    var hash = sha256(  result.username + '' +  result.password );
+    var crypto = require('crypto');
+    var Store = require('ministore')('db');
+    var Users = Store('users');
+    var challange = sha256( Math.random() + 'x' + Math.random() + new Date().getTime() + "Y" + self.counter + hash + result.displayName );
+    var user = { "username" : result.username , "hash" : hash , "challange" : challange , "displayName" : result.displayName }
+    Users.set(result.username , {""} );
     console.log('  name: ' + result.username);
-    console.log('  password: ' + result.password);
-    console.log('  rePassword: ' + result.rePassword);
+
   });

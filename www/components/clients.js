e7e950594c8900be6368809272aa319914f54e87
tholum <tholum@couleetechlink.com>
2015-10-01 11:17:53 -0500

    Pages

--- www/components/clients.js
+++ www/components/clients.js
@@ -2,6 +2,7 @@ componentToLoadClass = function( params ){
   var self = this;
   self.ovm = params.ovm;
   self.socket = params.socket;
+  self.subpage = ko.observable('clientList');
   self.allClients = ko.observableArray([]);
   self.socket.on("allClients" , function(data){
     self.allClients( data );
@@ -10,4 +11,9 @@ componentToLoadClass = function( params ){
   runOnLogin.push( function(){
     self.socket.emit("getOpenVPNConfigs");
   });
+  self.showWizard = ko.observable(false);
+  self.createClientConfig = function(params){
+    console.log( params );
+    socket.emit("createOpenVPNConfig" , params );
+  }
 }
